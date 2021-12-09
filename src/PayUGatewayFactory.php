41a9efacfcb0fdc3631419db72dd3f53f2dcac16
Jędrzej Jarocki <jedrzej.jarocki@inpay.pl>
2021-12-09 15:27:10 +0100

    test

--- src/PayUGatewayFactory.php
+++ src/PayUGatewayFactory.php
@@ -8,7 +8,7 @@
 
 declare(strict_types=1);
 
-namespace BitBag\SyliusPayUPlugin;
+namespace Fiberpay\SyliusFiberpayPlugin;
 
 use BitBag\SyliusPayUPlugin\Bridge\OpenPayUBridgeInterface;
 use Payum\Core\Bridge\Spl\ArrayObject;
