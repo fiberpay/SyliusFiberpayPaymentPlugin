1dd2b3d79a8d56e8911fb61993f6a3841e48a064
Jędrzej Jarocki <jedrzej.jarocki@inpay.pl>
2021-12-14 14:17:08 +0100

    extend FiberpayApi

--- src/FiberpayApi.php
+++ src/FiberpayApi.php
@@ -4,21 +4,75 @@ declare(strict_types=1);
 
 namespace Fiberpay\FiberpaySyliusPaymentPlugin;
 
+// use FiberPay\FiberPayClient;
+
 final class FiberpayApi
 {
-    const ENVIRONMENT_PRODUCTION = 'production';
+
+    const CURRENCY_PLN = 'PLN';
+
+    public static $validCurrencies = [ self::CURRENCY_PLN ];
+
     const ENVIRONMENT_SANDBOX = 'sandbox';
+    const ENVIRONMENT_PRODUCTION = 'production';
+
+    public static $validEnvironments = [
+        self::ENVIRONMENT_SANDBOX,
+        self::ENVIRONMENT_PRODUCTION,
+    ];
+
+    /** @var string */
+    private $environment;
 
     /** @var string */
     private $apiKey;
 
-    public function __construct(string $apiKey)
+    /** @var string */
+    private $secretKey;
+
+    /** @var string */
+    private $orderCode;
+
+    public function __construct(
+        string $environment,
+        string $apiKey,
+        string $secretKey,
+        string $orderCode
+    )
     {
+        $this->environment = $environment;
         $this->apiKey = $apiKey;
+        $this->secretKey = $secretKey;
+        $this->orderCode = $orderCode;
+    }
+
+    public function getEnvironment(): string
+    {
+        return $this->environment;
     }
 
     public function getApiKey(): string
     {
         return $this->apiKey;
     }
+
+    public function getSecretKey(): string
+    {
+        return $this->secretKey;
+    }
+
+    public function getOrderCode(): string
+    {
+        return $this->orderCode;
+    }
+
+    public function isSandbox()
+    {
+        return $this->environment === self::ENVIRONMENT_SANDBOX;
+    }
+
+    // public function getClientInstance()
+    // {
+        // return new FiberPayClient($this->apiKey, $this->secretKey, $this->isSandbox());
+    // }
 }
